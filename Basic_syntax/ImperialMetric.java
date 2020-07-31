8515c6556d8fff9f6fabd8e8d8d66d0bcb583344
Princess-luleigh <68966808+Princess-luleigh@users.noreply.github.com>
2020-07-31 21:12:55 +0200

    Add files via upload

---
+++ Basic_syntax/ImperialMetric.java
@@ -0,0 +1,119 @@
+package WEEK10;
+
+import java.util.Scanner;
+
+// main class
+
+public class ImperialMetric {
+
+// main method
+
+    public static void main(String[] args) {
+
+// variable declaration
+
+        int minNumber, maxNumber;
+
+        float meter;
+
+        float feetToMeter, inchToMeter;
+
+// scanner class object to take the user input
+
+        Scanner sc = new Scanner(System.in);
+
+// take first minimum number from user
+
+        System.out.println("Enter the minimum number of feet (not less than 0):");
+
+        minNumber = sc.nextInt();
+
+// take second maximum number from user
+
+        System.out.println("Enter the maximum number of feet (not more than 30):");
+
+        maxNumber = sc.nextInt();
+
+// declare a array for inch and feet
+
+        int[] inches = new int[12];
+
+// here we give the size max size of array
+
+        int[] feets = new int[maxNumber];
+
+        System.out.print("   |");
+
+// for loop to print the inch number form 0 to 11
+
+        for (int i = 0; i < 12; i++) {
+
+// %6s format string for spaces
+
+            System.out.print(String.format("%5s", i) + "\"");
+
+        }
+
+// new line
+
+        System.out.println();
+
+// first for loop for rows this loop start with minimum and end with maximum
+
+// value from user input
+
+        for (int i = minNumber; i <= feets.length; i++) {
+
+// print the value 0 to 30
+
+            if (i >= 0 && i <= 9) {
+
+                System.out.print(i + "' | ");
+
+            } else {
+
+                System.out.print(i + "'| ");
+
+            }
+
+// System.out.print(i+"'");
+
+// System.out.print(" | ");
+
+// for loop to calculate the meter value
+
+            for (int j = 0; j < inches.length; j++) {
+
+// convert feet into meter
+
+// 1 meter = 0.3048 meter
+
+                feetToMeter = (float) (i * 0.3048);
+
+// convert inch into meter
+
+// 1 inch = 0.0254 meter
+
+                inchToMeter = (float) (j * 0.0254);
+
+// add the feet and inch value to get the meter value
+
+                meter = feetToMeter + inchToMeter;
+
+// format and print the meter value to 3 decimal place by %.3f
+
+                System.out.printf("%.3f", meter);
+
+                System.out.print(" ");
+
+            }
+
+// new line
+
+            System.out.println(" ");
+
+        }
+
+    }
+
+}
---
+++ Basic_syntax/TimeConvertor.java
@@ -0,0 +1,45 @@
+package WEEK10;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+// Create The Class
+public class TimeConvertor {
+
+    // Create The Method
+    // to convert 12hr Time Format to 24hr Time Format
+    public static String _12to24 (String time) throws Exception {
+        SimpleDateFormat _12Format = new SimpleDateFormat("hh:mm a");
+        SimpleDateFormat _24Format = new SimpleDateFormat("HH:mm");
+        Date date = _12Format.parse(time);
+        return _24Format.format(date);
+    }
+
+    // Create The Method
+    // to convert 24hr Time Format to 12hr Time Format
+    public static String _24to12 (String time) throws Exception {
+        SimpleDateFormat _12Format = new SimpleDateFormat("hh:mm a");
+        SimpleDateFormat _24Format = new SimpleDateFormat("HH:mm");
+        Date date = _24Format.parse(time);
+
+        // Replacing AM/PM to am/pm
+        return _12Format.format(date).replace("AM", "am").replace("PM","pm");
+    }
+
+    // Create the Main Method
+    public static void main(String [] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a time ([h]h:mm [am|pm]):");
+        String time = sc.nextLine();
+
+        if (time.endsWith("pm") || time.endsWith("am"))
+            System.out.println(_12to24(time));
+        else
+            System.out.println(_24to12(time));
+    }
+}
+
+
+
+
