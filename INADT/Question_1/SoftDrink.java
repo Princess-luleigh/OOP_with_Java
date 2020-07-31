c35928fae80b1ab85cfa705c4c774512324b7096
Princess-luleigh <68966808+Princess-luleigh@users.noreply.github.com>
2020-07-31 21:51:35 +0200

    Add files via upload

---
+++ INADT/Question_1/SoftDrink.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public class SoftDrink{
+    String color, name;
+    double volume;
+    //volume.setScale(4);
+    SoftDrink(String n, String c, double v){
+        name = n;
+        color = c;
+        volume = v;
+    }
+}
+
+
+
